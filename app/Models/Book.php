618e7a3983095cc3fbca6f8b0e15b0fb91c5886f
77Ahmed <alainiahmed89@gmail.com>
2024-06-10 21:18:38 +0300

    start working in resource control

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -20,15 +20,35 @@ class Book extends Model
         return $query->where('title','LIKE','%'.$title.'%');
     }
     
-    public function scopePopular(Builder $query) : Builder
+    public function scopePopular(Builder $query, $from=null, $to =null ) : Builder
     {
-       return $query->withCount('reviews')
+       return $query->withCount([
+        'reviews'=>fn( Builder$q)=> $this->dataRangeFilter($q,$from,$to)
+        ])
                ->orderBy('reviews_count','desc');
     }
 
-    public function scopeHighesRated(Builder $query) : Builder
+    public function scopeHighesRated(Builder $query,$from=null,$to=null) : Builder
     {
-        return $query->withAvg('reviews','rating')
+        return $query->withAvg([
+            'reviews'=>fn(Builder $q)=> $this->dataRangeFilter($q,$from,$to)
+        ],'rating')
                 ->orderBy('reviews_avg_rating','desc'); 
+    }
+    
+    public function scopeNinReviews(Builder $query, int $minReviews){
+        return $query->havin('reciews_count','>=',$minReviews);
+    }
+    
+    private function dataRangeFilter(Builder $query, $from=null, $to =null){
+        if($from && !$to){
+            $query->where('created_at','>=','$from');
+         }
+         elseif(!$from && $to){
+             $query->where('created_at','<=','$to');
+         }
+         elseif($from && $to){
+             $query->whereBetween('created_at',['$from','$to']);
+         }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
-
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
+use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
 
 Route::get('/', function () {
 
     return ('Hi Everybody');
 });
+
+Route::resource('books',BookController::class );
